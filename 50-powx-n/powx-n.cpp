e296a8f87d015fa430cbbce65d56ee04ef3b2c20
Rishika140703 <117457996+Rishika140703@users.noreply.github.com>
2025-05-29 16:11:51 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (99.66%) - LeetSync

---
+++ 50-powx-n/powx-n.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    double myPow(double x, long long n) {
+        if(n==0) return 1;
+        if(n<0) return (double)(1/ myPow(x, -n));
+
+        double half = myPow(x, n/2);
+        if(n%2 == 0){
+            return half * half;
+        } else return half*half*x;
+       
+    }
+};
