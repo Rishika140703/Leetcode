acf23c8b297dcc55c4ec2189cc291b1b76db1a84
Rishika140703 <117457996+Rishika140703@users.noreply.github.com>
2025-10-19 22:29:00 +0530

    Time: 2 ms (95.58%) | Memory: 9 MB (92.92%) - LeetSync

--- 1747-lexicographically-smallest-string-after-applying-operations/lexicographically-smallest-string-after-applying-operations.cpp
+++ 1747-lexicographically-smallest-string-after-applying-operations/lexicographically-smallest-string-after-applying-operations.cpp
@@ -1,29 +1,33 @@
 class Solution {
 public:
-    void rotate(string &s, int b) {
-        reverse(begin(s), end(s));
-        reverse(begin(s), begin(s) + b);
-        reverse(begin(s) + b, end(s));
-    }
-
-    void dfs(string curr, int a, int b, unordered_set<string> &visited, string &smallestString) {
-        if (visited.count(curr)) return;
-        visited.insert(curr);
-        if (curr < smallestString) smallestString = curr;
-
-        string added = curr;
-        for (int i = 1; i < added.size(); i += 2)
-            added[i] = ((added[i] - '0' + a) % 10) + '0';
-        dfs(added, a, b, visited, smallestString);
-
-        rotate(curr, b);
-        dfs(curr, a, b, visited, smallestString);
-    }
-
     string findLexSmallestString(string s, int a, int b) {
-        unordered_set<string> visited;
-        string smallestString = s;
-        dfs(s, a, b, visited, smallestString);
-        return smallestString;
+        int n = s.size();
+        string res = s;
+        s = s + s;
+        int g = gcd(b, n);
+
+        auto add = [&](string& t, int start) {
+            int minVal = 10, times = 0;
+            for (int i = 0; i < 10; i++) {
+                int added = ((t[start] - '0') + i * a) % 10;
+                if (added < minVal) {
+                    minVal = added;
+                    times = i;
+                }
+            }
+            for (int i = start; i < n; i += 2) {
+                t[i] = '0' + ((t[i] - '0') + times * a) % 10;
+            }
+        };
+
+        for (int i = 0; i < n; i += g) {
+            string t = s.substr(i, n);
+            add(t, 1);
+            if (b % 2) {
+                add(t, 0);
+            }
+            res = min(res, t);
+        }
+        return res;
     }
 };
